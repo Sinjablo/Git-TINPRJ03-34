227f55cc636b2330155c5c1e10215b7c2ea17f44
Sinjablo <jarno.vuijk@gmail.com>
2021-03-10 23:30:29 +0100

    updated

---
+++ code/NodeMcu webserver testing/WebserverTesting/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <stdio.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <FS.h>
+#include <Wire.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+	Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
