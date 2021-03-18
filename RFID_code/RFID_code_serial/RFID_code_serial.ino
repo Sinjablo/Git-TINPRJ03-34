4537a1b0f2b6c675cfbc5b18ee792c844351daed
Itssantastic <sanderdeman2002@gmail.com>
2021-03-18 14:05:20 +0100

    kaula

---
+++ RFID_code/RFID_code_serial/RFID_code_serial.ino
@@ -0,0 +1,63 @@
+/*
+ * 
+ * "D4 7B DE 2A" is witte pas
+ * "F9 CE D1 B9" is blauwe RFID
+ * 
+ * 
+ * 
+ */
+ 
+#include <SPI.h>
+#include <MFRC522.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+ 
+void setup() 
+{
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.println("Houd uw pas bij de kaartlezer");
+  Serial.println();
+
+}
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "D4 7B DE 2A") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Pas is herkend, vul pincode in");
+    Serial.println();
+    delay(3000);
+  }
+ 
+ else   {
+    Serial.println(" Pas is niet herkend");
+    delay(3000);
+  }
+} 
