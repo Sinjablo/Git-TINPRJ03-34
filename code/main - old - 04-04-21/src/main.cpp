bf739e8c64fb6932f7ccc77e8c311bc1552a178d
Sinjablo <jarno.vuijk@gmail.com>
2021-04-04 23:14:52 +0200

    WDT bug fix

--- code/main - old - 04-04-21/src/main.cpp
+++ code/main - old - 04-04-21/src/main.cpp
@@ -1,5 +1,7 @@
 #include <arduino.h>
 #include <stdio.h>
+//#include <ESP8266WiFi.h>
+//#include <ESPAsyncTCP.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
@@ -34,6 +36,8 @@ char hexaKeys[ROWS][COLS] = {
   {'7', '8', '9', 'C'},
   {'*', '0', '#', 'D'}
 };
+//byte rowPins[ROWS] = {26, 25, 33, 32}; 
+//byte colPins[COLS] = {35, 34, 39, 36}; 
 byte rowPins[ROWS] = {23, 22, 3, 21}; 
 byte colPins[COLS] = {19, 18, 16, 17};
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
@@ -49,6 +53,10 @@ String dummyTempPasscode;
 String x;
 char g;
 const int tempBtn = 4;
+// Set LED GPIO
+const int ledPin = 2;
+// Stores LED state
+String ledState;
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
@@ -60,6 +68,9 @@ bool abortCheck = false;
 bool rfidCheck = false;
 int passcodeCheck = 0;
 String lastName = "Vuijk";
+bool withdrawCheck = false;
+bool balanceCheck = false;
+
 
 //--------------------------------------String return functions for the webserver to switch pages
 
@@ -120,8 +131,21 @@ String getPasscodeCheck(){
 	passcodeCheck = 0;
 	return String(tempPasscodeCheck);
 }
-/*
+String getLastName(){
+	return lastName;
+}
+String getWithdrawCheck(){
+	bool tempWithdrawCheck = withdrawCheck;
+	withdrawCheck = false;
+	return String(tempWithdrawCheck);
+}
+String getBalanceCheck(){
+	bool tempBalanceCheck = balanceCheck;
+	balanceCheck = false;
+	return String(tempBalanceCheck);
+}
 
+/*
 // ONLY FOR START-UP I THINK
 String processor(const String &var){
 	Serial.println(var);
@@ -144,6 +168,7 @@ String processor(const String &var){
 void setup(){
 	// Serial port for debugging purposes
 	Serial.begin(115200);
+	pinMode(ledPin, OUTPUT);
 	pinMode(tempBtn, OUTPUT);
 
 	// Initialize SPIFFS
@@ -193,7 +218,13 @@ void setup(){
 		request->send_P(200, "text/plain", getPasscodeCheck().c_str());
 		page = 1;
 	});
-  /*
+	server.on("/witdrawCheck", HTTP_GET, [](AsyncWebServerRequest *request) {
+		request->send_P(200, "text/plain", getBalanceCheck().c_str());
+	});
+	server.on("/balanceCheck", HTTP_GET, [](AsyncWebServerRequest *request) {
+		request->send_P(200, "text/plain", getWithdrawCheck().c_str());
+	});
+	/*
 	server.on("/passcodeLenght", HTTP_GET, [](AsyncWebServerRequest *request) {
 		request->send_P(200, "text/plain", getPasscodeLenght().c_str());
 	});
@@ -232,5 +263,17 @@ void loop(){
 		  	}
 	
   	}
+
+
+	switch (customKey)
+	{
+	case 'A':
+		/* code */
+		break;
+	
+	default:
+		break;
+	}
+
 	
 }
