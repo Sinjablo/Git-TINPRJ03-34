3c41b4183b09217c0023d6ceedae9e7c23fb3f12
Jarno <jarno.vuijk@gmail.com>
2021-05-27 16:32:44 +0200

    g

--- code/Main/src/main.cpp
+++ code/Main/src/main.cpp
@@ -575,6 +575,70 @@ void navigationInput(char customKey){
 	Serial.println(navigationKey);
 }
 
+void dispense50(int value) {
+  for (value; value != 0; value--) {
+    Serial.println("50 word uitgeworpen");
+    digitalWrite(motorForward50, HIGH);
+    delay(1300);
+    digitalWrite(motorForward50, LOW);
+    digitalWrite(motorBackward50, HIGH);
+    delay(800);
+    digitalWrite(motorBackward50, LOW);
+  }
+}
+void dispense20(int value) {
+  for (value; value != 0; value--) {
+    Serial.println("20 word uitgeworpen");
+    digitalWrite(motorForward20, HIGH);
+    delay(1500);
+    digitalWrite(motorForward20, LOW);
+    digitalWrite(motorBackward20, HIGH);
+    delay(800);
+    digitalWrite(motorBackward20, LOW);
+  }
+}
+void dispense10(int value) {
+  for (value; value != 0; value--) {
+    Serial.println("10 word uitgeworpen");
+    digitalWrite(motorForward10, HIGH);
+    delay(1500);
+    digitalWrite(motorForward10, LOW);
+    digitalWrite(motorBackward10, HIGH);
+    delay(800);
+    digitalWrite(motorBackward10, LOW); 
+  }
+}
+void dispense5(int value) {
+  for (value; value != 0; value--) {
+    Serial.println("5 word uitgeworpen");
+    digitalWrite(motorForward5, HIGH);
+    delay(1300);
+    digitalWrite(motorForward5, LOW);
+    digitalWrite(motorBackward5, HIGH);
+    delay(600);
+    digitalWrite(motorBackward5, LOW);
+  }
+}
+
+void dispenseMoney(){
+  for (int i = 2; i < 9; i = i + 2) {
+    if (noteArray[i] == 50) {
+      dispense50(noteArray[i - 1]);
+    } else if (noteArray[i] == 20) {
+      dispense20(noteArray[i - 1]);
+    } else if (noteArray[i] == 10) {
+      dispense10(noteArray[i - 1]);
+    } else if (noteArray[i] == 5) {
+      dispense5(noteArray[i - 1]);
+    } else if (noteArray[i] == 0) {
+      break;
+    }
+    noteArray[i] = 0;
+    noteArray[i - 1] = 0;
+  }
+	abortCheck = true;
+}
+
 void billSelection(){
 	//Serial.println(atoi(((String)billCombinations[comb].charAt(1)).c_str()));
 	int noteArrayConstructorStep = 0;
@@ -1008,69 +1072,9 @@ void customAmountMenu(char customKey){
 	}
 }
 
-void dispenseMoney(){
-  for (int i = 2; i < 9; i = i + 2) {
-    if (noteArray[i] == 50) {
-      dispense50(noteArray[i - 1]);
-    } else if (noteArray[i] == 20) {
-      dispense20(noteArray[i - 1]);
-    } else if (noteArray[i] == 10) {
-      dispense10(noteArray[i - 1]);
-    } else if (noteArray[i] == 5) {
-      dispense5(noteArray[i - 1]);
-    } else if (noteArray[i] == 0) {
-      break;
-    }
-    noteArray[i] = 0;
-    noteArray[i - 1] = 0;
-  }
-	abortCheck = true;
-}
 
-void dispense50(int value) {
-  for (value; value != 0; value--) {
-    Serial.println("50 word uitgeworpen");
-    digitalWrite(motorForward50, HIGH);
-    delay(1300);
-    digitalWrite(motorForward50, LOW);
-    digitalWrite(motorBackward50, HIGH);
-    delay(800);
-    digitalWrite(motorBackward50, LOW);
-  }
-}
-void dispense20(int value) {
-  for (value; value != 0; value--) {
-    Serial.println("20 word uitgeworpen");
-    digitalWrite(motorForward20, HIGH);
-    delay(1500);
-    digitalWrite(motorForward20, LOW);
-    digitalWrite(motorBackward20, HIGH);
-    delay(800);
-    digitalWrite(motorBackward20, LOW);
-  }
-}
-void dispense10(int value) {
-  for (value; value != 0; value--) {
-    Serial.println("10 word uitgeworpen");
-    digitalWrite(motorForward10, HIGH);
-    delay(1500);
-    digitalWrite(motorForward10, LOW);
-    digitalWrite(motorBackward10, HIGH);
-    delay(800);
-    digitalWrite(motorBackward10, LOW); 
-  }
-}
-void dispense5(int value) {
-  for (value; value != 0; value--) {
-    Serial.println("5 word uitgeworpen");
-    digitalWrite(motorForward5, HIGH);
-    delay(1300);
-    digitalWrite(motorForward5, LOW);
-    digitalWrite(motorBackward5, HIGH);
-    delay(600);
-    digitalWrite(motorBackward5, LOW);
-  }
-}
+
+
 
 void receiptMenu(char customKey){
 	switch (customKey){
